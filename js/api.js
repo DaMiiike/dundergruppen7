a3823f63ed80ab266cefd3af6360b150fc68c17b
mariarosariojson <mariarosariojohansson@gmail.com>
2021-12-27 11:06:19 +0100

    api added

--- js/api.js
+++ js/api.js
@@ -1,6 +1,7 @@
+
 const requestUrl = 'https://api.unsplash.com/search/photos?query=quokka-water-bottles&client_id=lRkZOGZxdjdeciqlUKhN8aEr1VV5G4glkVx_W-bsPNE';
 const imageButton = document.querySelector('.imageButton');
-const imageGallery = document.querySelector('.imageGallery');
+const imageGallery = document.querySelector('.img-gallery');
 
     imageButton.addEventListener('click', async () => {
       let randomImage = await newImage();
@@ -10,7 +11,7 @@ const imageGallery = document.querySelector('.imageGallery');
     async function newImage() {
       let randomNumber = Math.floor(Math.random() * 10);
       return fetch(requestUrl)
-        .then((response) => response.json.parse())
+        .then((response) => response.json())
         .then((data) => {
           let allImages = data.results[randomNumber];
           return allImages.urls.regular;
