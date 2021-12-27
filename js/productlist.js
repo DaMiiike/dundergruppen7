e1060d715ad6dfb50c26c7a7631229650f9111ee
Anna Attehed <anna.attehed@medieinstitutet.se>
2021-12-27 09:17:58 +0100

    hej

---
+++ js/productlist.js
@@ -0,0 +1,58 @@
+/* 
+function allProducts(products, htmlId) {
+
+    products.map(product) => {
+
+    }
+} */
+
+let productPage = document.querySelectorAll('');
+
+let cartProducts = [
+    {
+        name: 'Pink Wonder Bottle',
+        price: 299,
+        volume: '600 ml',
+        description: 'The Wonder Bottle is a 600 ml stainless steel thermal bottle that will keep its content warm and cold for longer than the average waterbottle can'
+            + 'It comes with an understated carrying handle to keep it easy on-the-go and a various choice of colors'
+            + 'Which one will be your new favorite to keep you hydrated all day every day?',
+        material: 'Produced in Sweden in stainless steel' + 'BPA-free & toxin free',
+        features: 'Capacity to keep both warm and cold beverages',
+        inCart: 0,
+    },
+
+    {
+        name: 'Blue Wonder Bottle',
+        price: 299,
+        volume: '600 ml',
+        description: 'The Wonder Bottle is a 600 ml stainless steel thermal bottle that will keep its content warm and cold for longer than the average waterbottle can'
+            + 'It comes with an understated carrying handle to keep it easy on-the-go and a various choice of colors'
+            + 'Which one will be your new favorite to keep you hydrated all day every day?',
+        material: 'Produced in Sweden in stainless steel' + 'BPA-free & toxin free',
+        features: 'Capacity to keep both warm and cold beverages',
+        inCart: 0,
+    },
+    {
+        name: 'Green Wonder Bottle',
+        price: 299,
+        volume: '600 ml',
+        description: 'The Wonder Bottle is a 600 ml stainless steel thermal bottle that will keep its content warm and cold for longer than the average waterbottle can'
+            + 'It comes with an understated carrying handle to keep it easy on-the-go and a various choice of colors'
+            + 'Which one will be your new favorite to keep you hydrated all day every day?',
+        material: 'Produced in Sweden in stainless steel' + 'BPA-free & toxin free',
+        features: 'Capacity to keep both warm and cold beverages',
+        inCart: 0,
+    },
+    {
+        name: 'Red Wonder Bottle',
+        price: 299,
+        volume: '600 ml',
+        description: 'The Wonder Bottle is a 600 ml stainless steel thermal bottle that will keep its content warm and cold for longer than the average waterbottle can'
+            + 'It comes with an understated carrying handle to keep it easy on-the-go and a various choice of colors'
+            + 'Which one will be your new favorite to keep you hydrated all day every day?',
+        material: 'Produced in Sweden in stainless steel' + 'BPA-free & toxin free',
+        features: 'Capacity to keep both warm and cold beverages',
+        inCart: 0,
+    },
+];
+
