ec59f0550ec5dbf21e56a7fcba39ace263b1daa0
mariarosariojson <mariarosariojohansson@gmail.com>
2021-12-27 09:32:37 +0100

    api js

---
+++ js/api.js
@@ -0,0 +1,18 @@
+const requestUrl = 'https://api.unsplash.com/search/photos?query=quokka-water-bottles&client_id=lRkZOGZxdjdeciqlUKhN8aEr1VV5G4glkVx_W-bsPNE';
+const imageButton = document.querySelector('.imageButton');
+const imageGallery = document.querySelector('.imageGallery');
+
+    imageButton.addEventListener('click', async () => {
+      let randomImage = await newImage();
+      imageGallery.src = randomImage;
+    });
+
+    async function newImage() {
+      let randomNumber = Math.floor(Math.random() * 10);
+      return fetch(requestUrl)
+        .then((response) => response.json.parse())
+        .then((data) => {
+          let allImages = data.results[randomNumber];
+          return allImages.urls.regular;
+        });
+    }
