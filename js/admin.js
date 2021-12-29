9aa536fc1585479c2eae270d97dc5e8e401cb045
astrid thil <astrid.thil@gmail.com>
2021-12-29 15:10:26 +0100

    admin js klar

--- js/admin.js
+++ js/admin.js
@@ -1,21 +1,29 @@
-const adminForm = document.querySelector(".admin-form")
+const adminForm = document.querySelector(".admin-form");
 const inputName = document.querySelector(".input-product-name").value;
 const inputDesc = document.querySelector(".input-product-desc").value;
 const inputPrice = document.querySelector(".input-product-price").value;
 const inputBtn = document.querySelector(".input-product-btn");
-
 const newProducts = document.querySelector(".newproducts");
 
-adminForm.addEventListener("submit", function(e) {
-    e.preventDefault();
-});
+
+let inputList = JSON.parse(localStorage.getItem("newProduct")) || [];
+
 
 inputBtn.addEventListener("click", function() {
     const inputName = document.querySelector(".input-product-name").value;
     const inputDesc = document.querySelector(".input-product-desc").value;
     const inputPrice = document.querySelector(".input-product-price").value; 
-    localStorage.setItem("newProduct", JSON.stringify({inputName, inputDesc, inputPrice}));
     
-    console.log(inputName)
+    let newProduct = {
+        name: inputName,
+        description: inputDesc,
+        price: inputPrice,
+        };
 
+        inputList.push(newProduct);
+ 
+    localStorage.setItem("newProduct", JSON.stringify(inputList));
+    
+   
 })
+
